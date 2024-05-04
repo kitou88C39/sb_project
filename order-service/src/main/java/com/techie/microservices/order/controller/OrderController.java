055c3b2bf2b68c8b311b3118645443e5db879307
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-04 11:40:41 +0900

    controller/OrderController.javaを作成する

--- order-service/src/main/java/com/techie/microservices/order/controller/OrderController.java
+++ order-service/src/main/java/com/techie/microservices/order/controller/OrderController.java
@@ -1,12 +1,21 @@
 package com.techie.microservices.order.controller;
 
+import com.techie.microservices.order.dto.OrderRequest;
+import com.techie.microservices.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+    }
 
 }
