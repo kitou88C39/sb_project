8d39e6b2d54b8729bc6c85ed0454ec17a3028d6a
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-04 11:07:29 +0900

    service/OrderService.javaを作成する

--- order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -2,6 +2,7 @@ package com.techie.microservices.order.service;
 
 import com.techie.microservices.order.dto.OrderRequest;
 import com.techie.microservices.order.model.Order;
+import com.techie.microservices.order.model.OrderRepository;
 
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,14 @@ import java.util.UUID;
 
 @Service
 public class OrderService {
+    private final OrderRepository orderRepository;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         order.setPrice(orderRequest.price());
         order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
 
     }
 
