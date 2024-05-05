4aaf7d4bc4031ec36c0cc3739d9885a2f95bc93d
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-05 16:05:57 +0900

    Java/com/teich/microservice/inventoryにcontroller/inventoryController.javaを作成する

--- inventory-service/src/main/java/com/techie/microservices/inventory/controller/inventoryController.java
+++ inventory-service/src/main/java/com/techie/microservices/inventory/controller/inventoryController.java
@@ -1,5 +1,10 @@
 package com.techie.microservices.inventory.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController("/api/inventory")
 public class inventoryController {
 
 }
