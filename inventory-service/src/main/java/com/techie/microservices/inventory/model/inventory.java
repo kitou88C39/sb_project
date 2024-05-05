b259dee5afcca616d36276e20f9579323341f2f1
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-05 15:10:19 +0900

    java/com/teich/microservice/inventoryにmodelファイルinventory.javaを作成する

--- inventory-service/src/main/java/com/techie/microservices/inventory/model/inventory.java
+++ inventory-service/src/main/java/com/techie/microservices/inventory/model/inventory.java
@@ -2,13 +2,17 @@ package com.techie.microservices.inventory.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "t_inventory")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class inventory {
 
 }
