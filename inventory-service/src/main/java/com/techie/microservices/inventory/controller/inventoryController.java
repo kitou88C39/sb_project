c26d4d13063edecfa9860d0e0fe39d1da272e01c
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-05 16:24:10 +0900

    Java/com/teich/microservice/inventoryにcontroller/inventoryController.javaを作成する

--- inventory-service/src/main/java/com/techie/microservices/inventory/controller/inventoryController.java
+++ inventory-service/src/main/java/com/techie/microservices/inventory/controller/inventoryController.java
@@ -3,10 +3,7 @@ package com.techie.microservices.inventory.controller;
 import com.techie.microservices.inventory.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,5 +15,7 @@ public class inventoryController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
 
+    }
 }
