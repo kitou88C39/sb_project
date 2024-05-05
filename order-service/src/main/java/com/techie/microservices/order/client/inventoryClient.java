7d4d475f78006b6dcb3ead4c12a04a846cc84d90
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-05 20:52:19 +0900

    Java/com/teich/microservice/orderにclientフォルダとinventoryClient.javaを作成する

--- order-service/src/main/java/com/techie/microservices/order/client/inventoryClient.java
+++ order-service/src/main/java/com/techie/microservices/order/client/inventoryClient.java
@@ -1,5 +1,8 @@
 package com.techie.microservices.order.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(value = "inventory", url = "http://localhost:8082")
 public class inventoryClient {
 
 }
