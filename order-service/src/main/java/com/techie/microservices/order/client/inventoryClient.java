d4ecdddecf3496173378d29c155f3e47d8eb171d
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-06 15:22:08 +0900

    Contract Stub Runnerをpom.xmlに追加する

--- order-service/src/main/java/com/techie/microservices/order/client/inventoryClient.java
+++ order-service/src/main/java/com/techie/microservices/order/client/inventoryClient.java
@@ -2,6 +2,7 @@ package com.techie.microservices.order.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "inventory", url = "http://localhost:8082")
