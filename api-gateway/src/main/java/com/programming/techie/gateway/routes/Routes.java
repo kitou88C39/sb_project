65ae149c0a8cdde0a52f350ba8aa65bd841f7838
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-07 20:58:30 +0900

    main/java/com/programming/techie/gatewayにroutesフォルダにRoutes.javaを作成する

--- api-gateway/src/main/java/com/programming/techie/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/programming/techie/gateway/routes/Routes.java
@@ -1,8 +1,11 @@
 package com.programming.techie.gateway.routes;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Routes {
+
+    @Bean
 
 }
