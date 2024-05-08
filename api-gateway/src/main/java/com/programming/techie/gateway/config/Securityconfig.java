3c00f70a369a580244d47cff2c95b2743f52625c
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-08 21:18:33 +0900

    main/java/com/programming/techie/gatewayにconfigフォルダとSecurityconfig.javaを作成する

--- api-gateway/src/main/java/com/programming/techie/gateway/config/Securityconfig.java
+++ api-gateway/src/main/java/com/programming/techie/gateway/config/Securityconfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class Securityconfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
                 .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
