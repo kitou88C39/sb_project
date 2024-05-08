b0aeead7a501d6b15ed6d9f819aed581fc9274f1
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-08 21:02:20 +0900

    main/java/com/programming/techie/gatewayにconfigフォルダとSecurityconfig.javaを作成する

--- api-gateway/src/main/java/com/programming/techie/gateway/config/Securityconfig.java
+++ api-gateway/src/main/java/com/programming/techie/gateway/config/Securityconfig.java
@@ -1,8 +1,13 @@
 package com.programming.techie.gateway.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.boot.context.annotation.Configuration;
 
 @Configuration
 public class Securityconfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain()
 
 }
